e943c8d4103f595c67b04a99ff320c050532947a
suman <sumankumarghosh94@gmail.com>
2025-02-21 23:32:54 +0530

    feat: added loader

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,28 @@
 'use client';
 import Description from '@/components/client/description/description';
 import Landing from '@/components/client/landing/landing';
+import Loader from '@/components/client/preloader/loader';
+import { AnimatePresence } from 'framer-motion';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
 
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log('Setting isLoading to false');
+      setIsLoading(false);
+      document.body.style.cursor = 'default';
+      window.scrollTo(0, 0);
+    }, 2000);
+  }, []);
+
   return (
-    <main>
+    <main data-scroll-container>
+      <AnimatePresence mode='wait'>
+        {isLoading && <Loader />}
+      </AnimatePresence>
       <Landing />
       <Description />
     </main>
