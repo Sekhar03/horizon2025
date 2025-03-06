4d38ed52ffcbf4a54ff7978c91669e21f03157c5
subhunath <subhashrinath10@gmail.com>
2025-03-06 11:21:48 +0530

    commit

--- components/client/testimonial/testimonial.tsx
+++ components/client/testimonial/testimonial.tsx
@@ -32,21 +32,17 @@ const data = [
 
         <h3 className="text-lg md:text-2xl font-bold text-blue-600">Fun Events</h3>
         <ul className="list-disc pl-5 text-gray-300">
-          <li>Scavenger Hunt (10:00 AM - Dome)</li>
-          <li>Mystery Room (11:00 AM - Educate Room)</li>
-          <li>AI Art Competition (Online)</li>
+          <li>Gully Cricket (8:00 AM - MP Memorial Stadium)</li>
+          <li>Mystery Room (10:00 AM - Old Building,Room 204,205)</li>
           <li>Digital Duel (Online)</li>
-          <li>Tug Of War (2:00 PM - Production Seminar Hall)</li>
-          <li>Paper Dance (2:00 PM - Auditorium)</li>
           <li>Brust The Balloon (2:00 PM - Dome)</li>
-          
         </ul>
 
         <h3 className="text-lg md:text-2xl font-bold text-blue-600">Literary Events</h3>
         <ul className="list-disc pl-5 text-gray-300">
             <li>Doodling (11:00 AM - New Building, Room 103)</li>
             <li>Wordless Wonders (8:00 AM - Dome)</li>
-             <li>Mun (9:00 AM - Edusat Room)</li>
+            <li>Mun (9:00 AM - Edusat Room)</li>
         </ul>
         
         <h3 className="text-lg md:text-2xl font-bold text-blue-600">Workshops</h3>
@@ -79,11 +75,9 @@ const data = [
 
         <h3 className="text-lg md:text-2xl font-bold text-blue-600">Fun Events</h3>
         <ul className="list-disc pl-5 text-gray-300">
-          <li>Rubik&apos;s Cube (9:00 AM - Educate Room)</li>
-          <li>Logo Design (10:00 AM - Dome)</li>
-          <li>Doodling (11:00 AM - New Building, Room 103)</li>
-          <li>Grafitti (2:00 PM - Old Building)</li>
-          <li>Photo Of The Day (Online)</li>
+          <li>Scavenger Hunt (11:00 AM - New Building)</li>
+          <li>Rubiks Cube (9:00 AM - New Building, Room 204,205)</li>
+          <li>Mystery Room (10:00 AM - Old Building,Room 204,205)</li>  
         </ul>
 
         <h3 className="text-lg md:text-2xl font-bold text-blue-600">Literary Events</h3>
@@ -124,10 +118,17 @@ const data = [
 
         <h3 className="text-lg md:text-2xl font-bold text-blue-600">Fun Events</h3>
         <ul className="list-disc pl-5 text-gray-300">
-          <li>Gully Cricket (8:00 AM - MP Memorial Stadium)</li>
-          <li>Destiny Duo (9:00 AM - New Building)</li>
-          <li>Short Film Competition (10:00 AM)</li>
-          <li>Wordless Wonders (8:00 AM - Dome)</li>
+          <li>Destiny Duo (9:00 AM - New Building,Room 103)</li>
+          <li>Paper Dance (02:00 PM - Dome)</li>
+          <li>Tug of War (8:00 AM - MP Memorial Stadium)</li>
+        </ul>
+
+        <h3 className="text-lg md:text-2xl font-bold text-blue-600">Literary Events</h3>
+        <ul className="list-disc pl-5 text-gray-300">
+          <li>Quiz</li>
+          <li>Wordsmith Maze</li>
+          <li>Identity insight</li>
+          <li>Trivia Quiz(12pm)</li>
         </ul>
 
         <h3 className="text-lg md:text-2xl font-bold text-blue-600">Workshops</h3>
